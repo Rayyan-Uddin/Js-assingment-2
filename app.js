5d2904347ca69031d9061e29cafd7e65ed59a13e
Rayyan-Uddin <rayyanuddin007@gmail.com>
2023-01-30 02:52:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+// Js assingment 2 
+
+//---------------------------------- Chapter 2
+
+
+// Question 1
+// var username;
+
+// Question 2
+// var myName="Rayyan Uddin";
+
+// Question 3
+// var message = "Hello World";
+// alert(message);
+
+// Question 4
+// var name = "Rayyan Uddin";
+// alert(name);
+
+// var age = "20 years old"
+// alert(age);
+
+// var course = "Certified Mobile Application Development"
+// alert(course);
+
+// Question 5
+// var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P ";
+// alert(pizza);
+
+// Queston 6
+// var email = " Rayyanuddin007@gmail.com";
+// var text = "My email address is";
+// alert(text+email);
+
+// Question 7
+// var text = "I am trying to learn from the book ";
+// var book = "A smarter way to learn JavaScript.";
+// alert(text+book);
+
+// Question 8
+// document.write("<h1>Yah! I can write HTML content through JavaSript</h1>")
+
+// Qustion 9
+// var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(message);
+
+// --------------------------END--------------------------------
+
+// Js assingment 2 
+
+// -----------------------------Chapter 3
+
+
+// Question 1
+// var age = 20;
+// alert("I am "+age+" Year's Old")
+
+// Question 2
+// var visits = 25;
+// alert("You Have visited this site "+visits+" times")
+
+// Question 3
+// var birthYear = 2003;
+// document.write("<h3> My birth year is " +birthYear+  "<br> Data type of my declared varible is Number </h3> </br>")
+
+// Question 4
+// var visitorName = "John doe";
+// var product = "T-Shirt";
+// var quantity = 5;
+// document.write(visitorName + " Ordered " + quantity +" "+ product + "(s) on XYZ clothing store.")
+
+//-------------------------------------END
+
+// Js assignment 2
+
+//----------------------------------Chapter 4
+
+
+// Question 1
+// var [name,age,gender]= ["Rayyan",20,"male"];
+// alert(name);
+
+// Quesiton 2
+// Legal variables
+// 1. myVariable
+// 2. my_varible
+// 3. _myVariable
+// 4. $myVariable
+// 5. $my_variable
+
+// Illegal variable
+// 1. myvariable
+// 2. 1myVariable
+// 3. .myvariable
+// 4. MYVARIABLE
+// 5. ~myVariable
+
+// Question 3
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("<h3>Variable names can only contain.numbers.$ and _. For example $my_1stVariable</h3>")
+// document.write("<h3>Variable mus begin with a letter, $ or _. For example $name, _name, name</h3>")
+// document.write("<h3>Variable names are case sensitive</h3>")
+// document.write("<h3>Variable names should not be JS keywords</h3>")
+
+//----------------------------------END
